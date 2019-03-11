08feb407661651443c04a7aaf4ea4afe40e603e2
MohitSojitra <mohitsojitra0001@gmail.com>
2019-03-11 18:49:40 +0530

    Update v1.1

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # WAAutoResponder
-WhatsApp Chat AutoResponder Bot
+
+WAAutoResponder is WhatsApp Chat Auto Reply Bot made for Automation with WhatsApp. It help to Add custom commands for reply.
+
+# Features
+- Easy to Use
+- No Root Required
+- Customisable
+
+# How it Works
+
+WAAutoResponder uses Notification Apis of Android to Reply through Notifications of Received Messages. So Make Sure Chats Are not Mute.
+
+# How to Use
+
+Just Build and Install it on your phone along with WhatsApp and Open it.
+
+# Future Updates
+
+Non Rooted Version have its own remedies so in future i might come up with Rooted Version.
+Rooted Version will have more Access to WhatsApp Data to add some cool new Options.
+
+# Any Issues?
+
+Yes its not complete yet. Access to whatsapp chat data is limited right now.
+
+# **Note**:
+
+- This project is NOT affiliated or approved by WhatsApp Inc.
+- As you might know WhatsApp Inc. is not supporting any kind of Bot though WhatsApp Business Version have its own bot apis, so there are chances of Ban using it. 
+But as It is repling through Notification it act as legit action so chances are less.
+
+License
+----
+
+GNU General Public License v3.0
--- app/src/main/java/com/kmods/waautoresponder/NotificationReceiver.java
+++ app/src/main/java/com/kmods/waautoresponder/NotificationReceiver.java
@@ -1,7 +1,9 @@
 package com.kmods.waautoresponder;
 
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -13,11 +15,19 @@ import android.widget.Toast;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Objects;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.RemoteInput;
 
 public class NotificationReceiver extends NotificationListenerService {
     static boolean isRunning = false;//is Running Bool
     static Notification notification;
+    static Bundle bundle;
+    static ArrayList<RemoteInput> remoteInputs;
 
     SharedPreferences sharedPreferences;
 
@@ -32,13 +42,60 @@ public class NotificationReceiver extends NotificationListenerService {
                     try{
                         notification = sbn.getNotification();//Latest Notification of WhatsApp
                         if(notification != null){
-                            Bundle bundle = notification.extras;
-                            logBundle(bundle);
-                            toastMsg("WAAutoResponder Read Notification");
+                            bundle = notification.extras;
+                            //logBundle(bundle);
+                            remoteInputs = getRemoteInputs(notification);
+                            if(remoteInputs != null && remoteInputs.size() > 0){
+                                Object isGroupConversation = bundle.get("android.isGroupConversation");
+                                String conversationTitle = bundle.getString("android.conversationTitle");
+
+                                if(isGroupConversation != null){
+                                    boolean isGroup = (((boolean) isGroupConversation) && (conversationTitle != null));//Group Params
+                                    Object title = bundle.get("android.title");//Chat Title
+                                    Object text = bundle.get("android.text");//Chat Text
+
+                                    if(title != null && text != null) {
+                                        //Common Replies
+                                        if (text.equals("@helpkp")) {
+                                            sendMsg("*KP's Bot Commands* :-\n\n@emailkp - to get my official EMail Address.\n\n@webkp - to get my offical website link.");
+                                        } else if (text.equals("@emailkp")) {
+                                            sendMsg("*KP's Official EMail* :- \n\npatel.kuldip91@gmail.com");
+                                        } else if (text.equals("@webkp")) {
+                                            sendMsg("*KP's Official Website* :- \n\nhttps://kuldippatel.dev");
+                                        }
+
+                                        //Group Specific Replies
+                                        if(isGroup) {
+                                            String[] title_split = parseTitle(String.valueOf(title));
+                                            String group_name = title_split[0];
+                                            String sender = title_split[(title_split.length - 1)];
+
+                                            if(text.equals("@warnings")){
+                                                sendMsg("Hello " +
+                                                        sender +
+                                                        " :-\n\n" +
+                                                        "Sorry But This Feature is Still Under Construction!");
+                                            }
+
+                                            //for Comp162 and 16-Comp Group
+                                            if(String.valueOf(title).contains("Comp162") || String.valueOf(title).contains("16-Comp")) {
+                                                if (text.equals("@help")) {
+                                                    sendMsg("*Comp162 Group Commands* :-\n" +
+                                                            "\n" +
+                                                            "@goodmorning and @goodnight - to greet group Members.");
+                                                } else if(text.equals("@goodmorning")){
+                                                    sendMsg("Good Morning All!");
+                                                } else if(text.equals("@goodnight")){
+                                                    sendMsg("Good Night All!");
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
                         }
                     } catch (Exception e){
                         notification = null;
-                        toastMsg("WAAutoResponder Fails to Read Last Notification!");
                         e.printStackTrace();
                     }
                 }
@@ -67,10 +124,75 @@ public class NotificationReceiver extends NotificationListenerService {
     }
 
     /*
-     * Toast Received Message
+     * get Remote Input of Notification
      */
-    private void toastMsg(String s){
-        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    private ArrayList<RemoteInput> getRemoteInputs(Notification notification){
+        ArrayList<RemoteInput> remoteInputs = new ArrayList<>();
+        NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender(notification);
+        for(NotificationCompat.Action act : wearableExtender.getActions()) {
+            if(act != null && act.getRemoteInputs() != null) {
+                remoteInputs.addAll(Arrays.asList(act.getRemoteInputs()));
+            }
+        }
+        return remoteInputs;
+    }
+
+    /*
+     * Send/Reply through Notification
+     */
+    private void sendMsg(String msg){
+        RemoteInput[] allremoteInputs = new RemoteInput[remoteInputs.size()];
+        Intent localIntent = new Intent();
+        localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Iterator it = remoteInputs.iterator();
+        int i=0;
+        while (it.hasNext()) {
+            allremoteInputs[i] = (RemoteInput) it.next();
+            bundle.putCharSequence(allremoteInputs[i].getResultKey(), msg);//This work, apart from Hangouts as probably they need additional parameter (notification_tag?)
+            i++;
+        }
+        RemoteInput.addResultsToIntent(allremoteInputs, localIntent, bundle);
+        try {
+            Objects.requireNonNull(replyAction(notification)).actionIntent.send(this, 0, localIntent);
+        } catch (PendingIntent.CanceledException e) {
+            Log.e(Const.LOG, "replyToLastNotification error: " + e.getLocalizedMessage());
+        }
+    }
+
+    /*
+     * Returns Reply Action of Notification
+     */
+    private NotificationCompat.Action replyAction(Notification notification) {
+        NotificationCompat.Action action;
+        for (NotificationCompat.Action action2 : new NotificationCompat.WearableExtender(notification).getActions()) {
+            if (isAllowFreeFormInput(action2)) {
+                return action2;
+            }
+        }
+        if (!(notification == null || notification.actions == null)) {
+            for (int i = 0; i < NotificationCompat.getActionCount(notification); i++) {
+                action = NotificationCompat.getAction(notification, i);
+                if (isAllowFreeFormInput(action)) {
+                    return action;
+                }
+            }
+        }
+        return null;
+    }
+
+    /*
+     * Checks for Text Input
+     */
+    private boolean isAllowFreeFormInput(NotificationCompat.Action action) {
+        if (action.getRemoteInputs() == null) {
+            return false;
+        }
+        for (RemoteInput allowFreeFormInput : action.getRemoteInputs()) {
+            if (allowFreeFormInput.getAllowFreeFormInput()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /*
@@ -136,5 +258,20 @@ public class NotificationReceiver extends NotificationListenerService {
         android.remoteInputHistory | null
         android.isGroupConversation | false
         */
+    }
+
+    /*
+     * Parse Group title with Combined Messages
+     */
+    private String[] parseTitle(String title){
+        String[] title_split = String.valueOf(title).split("[^\\s\\w\\d]");
+        StringBuilder builder = new StringBuilder();
+        for(int i=0;i< title_split.length;i++){
+            if(i>0){
+                builder.append(",");
+            }
+            builder.append(title_split[i]);
+        }
+        return builder.toString().split(",");
     }
 }
